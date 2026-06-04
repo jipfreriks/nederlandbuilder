64c4f0cd2b838ae745f8c5cd9fd40a89424a71e1
jipfreriks <jipjwfreriks@gmail.com>
2026-06-05 00:48:23 +0200

    mobile improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-import type { Metadata } from "next";
-
 export const metadata: Metadata = {
   title: "De18MiljoensteBondscoach.nl",
   description: "Maak jouw ideale Oranje-opstelling en deel hem met vrienden.",
